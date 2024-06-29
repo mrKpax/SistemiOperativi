c524470799eafb80f37e33142b5ba7e28f9b9c4b
Francesco Passiatore <94631872+mrKpax@users.noreply.github.com>
2024-06-29 16:35:28 +0200

    Update figli.c

--- figli.c
+++ figli.c
@@ -10,58 +10,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/signal.h>
-#include <signal.h>
 #include <unistd.h>
-
-pid_t F1, F2;
-
-void figlio1(void)
-{
-    printf("PID F1: %d\n", getpid());
-    printf("PID padre di F1: %d\n", getppid());
-}
-
-void figlio2(void)
-{
-    int sum = getpid() + getppid();
-    printf("Somma PID di F2 (%d) + PID padre (%d) = %d\n", getpid(), getppid(), sum);
-}
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-    int F1, F2, status1, status2;
+    int F1, F2, stat;
     
     F1 = fork();
     if (F1 < 0)
     {
-        fprintf(stderr, "Errore nella creazione di un figlio\n");
-        return -1;
+        fprintf(stderr, "Errore\n");
+        exit(-1);
     }
     else if (F1 == 0)
     {
-        figlio1();
+        printf("Sono F1 [PID %d], mio Padre ha PID [%d]\n", getpid(), getppid());
         exit(0);
     }
-    if (F1 > 0)
+    else if (F1 > 0)
     {
         F2 = fork();
         if (F2 < 0)
         {
-            fprintf(stderr, "Errore nella creazione di un figlio\n");
-            return -1;
+            fprintf(stderr, "Errore\n");
+            exit(-1);
         }
-        
         else if (F2 == 0)
         {
-            figlio2();
+            int sum;
+            sum = getpid() + getppid();
+            printf("Sono F2 [PID %d], SOMMA = %d\n", getpid(), sum);
+            exit(0);
+        }
+        else
+        {
+            wait(&stat);
+            wait(&stat);
+            printf("F1 [%d] ed F2 [%d] terminati\n", F1, F2);
             exit(0);
         }
     }
-    
-    waitpid(F1, &status1, 0);
-    waitpid(F2, &status2, 0);
-    printf("F1 con PID %d e F2 con PID %d terminati.\n", F1, F2);
-    return 0;
 }
